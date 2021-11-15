02bca3633d6ffd07447d90de2638a212593f0a76
Afonso Rodrigues <afonso.rodrigues@xpand-it.com>
2021-11-15 19:47:00 +0000

    Finalizar o projecto

--- src/application/Program.java
+++ src/application/Program.java
@@ -21,7 +21,26 @@ public class Program {
     System.out.print("Quantity: ");
     product.quantity = sc.nextInt();
     
-    System.out.println(product.toString());
+    System.out.println();
+    System.out.println("Product data: " + product.toString());
+    
+    System.out.println();
+    System.out.print("Enter the number of products to be added in stock: ");
+    int quantity = sc.nextInt();
+    product.addProducts(quantity);
+    
+    System.out.println();
+    System.out.println("Updated data: " + product.toString());
+    
+    System.out.println();
+    System.out.print("Enter the number of products to be removed in stock: ");
+    quantity = sc.nextInt();
+    product.removeProducts(quantity);
+    
+    System.out.println();
+    System.out.println("Updated data: " + product.toString());
+    
+    
     
     
     
