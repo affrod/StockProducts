9fb217063bf26622b7a5efd4725c1f649c5a9b45
Afonso Rodrigues <afonso.rodrigues@xpand-it.com>
2021-11-16 14:21:36 +0000

    Adicionar construtor à classe Product

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,22 +11,23 @@ public class Program {
     
     Locale.setDefault(Locale.US);
     Scanner sc = new Scanner(System.in);
-
-    Product product = new Product();
+    
     System.out.println("Enter product data: ");
     System.out.print("Name: ");
-    product.name = sc.nextLine();
+    String name = sc.nextLine();
     System.out.print("Price: ");
-    product.price = sc.nextDouble();
+    double price = sc.nextDouble();
     System.out.print("Quantity: ");
-    product.quantity = sc.nextInt();
+    int quantity = sc.nextInt();
+
+    Product product = new Product(name, price, quantity);
     
     System.out.println();
     System.out.println("Product data: " + product.toString());
-    
+    ;
     System.out.println();
     System.out.print("Enter the number of products to be added in stock: ");
-    int quantity = sc.nextInt();
+    quantity = sc.nextInt();
     product.addProducts(quantity);
     
     System.out.println();
@@ -39,10 +40,6 @@ public class Program {
     
     System.out.println();
     System.out.println("Updated data: " + product.toString());
-    
-    
-    
-    
     
     sc.close();
   } 
