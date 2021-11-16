a94764aa1d2d96217088ce3704592bdb71691c7d
Afonso Rodrigues <afonso.rodrigues@xpand-it.com>
2021-11-16 15:13:12 +0000

    Introduzir o encapsulamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,6 +22,11 @@ public class Program {
 
     Product product = new Product(name, price);
     
+    product.setName("Samsung");
+    System.out.println("Update name: " + product.getName());
+    product.setPrice(1200.00);
+    System.out.println("Update price: " + product.getPrice());
+    
     System.out.println();
     System.out.println("Product data: " + product.toString());
     ;
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -1,9 +1,9 @@
 package entities;
 
 public class Product {
-  public String name;
-  public double price;
-  public int quantity;
+  private String name;
+  private double price;
+  private int quantity;
   
   public Product() {
   }
@@ -21,6 +21,27 @@ public class Product {
   }
   
   
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+
   public double totalValueInStock() {
     return price * quantity;
   }
